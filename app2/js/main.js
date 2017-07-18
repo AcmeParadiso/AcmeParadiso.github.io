c62795a329f3cc277edd0290188171cafe571ff5
AcmeParadiso <Martesbs8818@student.laccd.edu>
2017-07-17 20:07:02 -0700

    Add files via upload

---
+++ app2/js/main.js
@@ -0,0 +1,37 @@
+
+
+//home
+var link = document.createElement( "link" );
+link.href = "css/normalize.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+link.media = "screen,print";
+
+document.getElementsByTagName( "head" )[0].appendChild( link );
+
+
+//home
+var link = document.createElement( "link" );
+link.href = "css/home.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+link.media = "screen,print";
+
+document.getElementsByTagName( "head" )[0].appendChild( link );
+
+
+$('#twinkling').css('height', 200);
+
+
+$('.ham-cont').click(function(){
+    $('.nav').animate({"left":"0%"}, "slow");
+    $('body').css({"overflow-y": "hidden"});
+})
+
+$('.pop-out-close').click(function(){
+    $('.nav').animate({"left":"100%"}, "slow");
+     $('body').css({"overflow-y": "visible"});
+})
+    
+
+
